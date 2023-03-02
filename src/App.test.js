526121d2b986319746bcab5779e27eca11e9b7d2
Andys-in <andyjeffersonperez@gmail.com>
2023-03-01 22:16:24 -0500

    beforeeach added

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,11 @@
-
-
-
+beforeEach(() =>{
+    describe('receives data from Github', () => {
+        test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+            fetch.mockResponseOnce(JSON.stringify({name: 'coder'}))
+            render(<App />)
+            const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+            expect(gitHubName).toHaveTextContent('coder')
+          })
+        
+    })
+})
---
+++ src/setup.Tests.js
@@ -0,0 +1,4 @@
+require('jest-fetch-mock').enableMocks()
+
+
+// curl https://api.github.com/users/YOURGITHUBUSERNAME
